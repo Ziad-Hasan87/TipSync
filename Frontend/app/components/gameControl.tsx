da26ffc7d00d18ed1c7e664cc73e8768b4fe26a5
Ziad-Hasan87 <141658254+Ziad-Hasan87@users.noreply.github.com>
2025-10-18 15:24:27 +0600

    small debugginh

--- Frontend/app/components/gameControl.tsx
+++ Frontend/app/components/gameControl.tsx
@@ -26,7 +26,7 @@ export default function GameControl({
 
     return (
         <div className="w-full h-full">
-            <div className="absolute bottom-55 w-1/2 flex items-center justify-center p-4 shadow-md z-5">    
+            <div className="absolute bottom-55 w-1/2 flex items-center justify-center p-4 shadow-md z-10">    
                 <KeySequence keyrows={keyrows} />
             </div>
             <div className="absolute bottom-35 bg-[wheat] w-1/2 flex items-center justify-center z-50">
